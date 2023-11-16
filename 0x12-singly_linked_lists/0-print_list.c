5b241bda2428ec95c42babec3c49a5b32a976f72
musafa21 <bsammy158@gmail.com>
2023-11-16 15:23:35 -0800

    Write a function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements in a linked list
+ * @h: the pointer to struct list_t to print
+ *
+ * Return: the total number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t total = 0;
+
+	while (h)
+	{
+		total++;
+		h = h->next;
+	}
+	return (total);
+}
